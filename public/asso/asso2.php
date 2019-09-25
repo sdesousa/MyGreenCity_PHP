29b8597626cd11054583c5e5ffe48918d37de999
sdesousa <sylvaindesousa@free.fr>
2019-09-25 15:49:43 +0200

    file hierarchy rework

---
+++ public/asso/asso2.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0' />
+        <title>My Green City - Conservatoire d'espaces naturels Centre-Val de Loire</title>
+        <link rel="stylesheet" href="style-asso.css">
+        <link href="https://fonts.googleapis.com/css?family=Manjari" rel="stylesheet">
+        <link rel="icon" type="image/png" href="images/green-v2.png" />
+    </head>
+    <body>
+        <header>
+            <nav>
+                <a class="menu_icon_link" href="homepage.html"> <img class="menu_icon" src="images/green-v2.png" alt="menu"> </a>
+                <div class="menu" id="link_burger" >
+                    <a class="close_burger" href="#"> <img src="images/Close_icon.png" alt="close"> </a>
+                    <a href="homepage.html#link_actions"> ACTIONS </a>
+                    <a href="#sub_menu"> ASSOCIATIONS </a>
+                    <a href="homepage.html#link_dechets"> DECHETS </a>
+                </div>
+                <a class="burger" href="#link_burger"> <img src="images/burger.svg" alt="menu"> </a>
+            </nav>
+
+            <div class="sub_menu_asso" id="sub_menu">
+                <a class="close" href="#"> <img src="images/Close_icon.png" alt="close"> </a>
+                <a href="homepage.html#link_associations">LES ASSOCIATIONS </a>
+                <a href="asso1.html"> CEN </a>
+                <a href="asso2.html"> LNE </a>
+                <a href="asso3.html"> LPB </a>
+            </div>
+        </header>
+
+        <section>
+            <h1>Loiret Nature Environnement</h1>
+        </section>
+
+        <section class="secInfo">
+            <h2>INFORMATIONS</h2>
+            <div class="Informations">
+                <div class="divTextInformations">
+                    <ul class ="textInformations">
+                        <li>Siège social</li>
+                        <li>64, Route d'Olivet - 45100 Orléans</li>
+                        <li>Télephone : 02 38 56 69 84 - Télécopie : 02 38 56 33 48</li>
+                        <li>Mail : asso@lne45.org</li>
+                        <li><a href="http://www.loiret-nature-environnement.org" target="_blank">www.loiret-nature-environnement.org</a></li>
+                    </ul>
+                </div>
+                <img class="assoImg" src="images/asso2.jpg" alt="Logo Loiret Nature Environnement">
+            </div>
+        </section>
+
+        <section class="description">
+            <h2>DESCRIPTION</h2>
+            <div class="textDescription">
+                <p>Créée en 1946 sous le nom des "Naturalistes Orléanais", Loiret Nature Environnement est une association de loi 1901.</p>
+                <p>Elle a pour but de: Mieux connaitre pour mieux préserver, Eduquer à l'environnement pour sensibiliser un large public et Agir pour plus d'éco-citoyenneté.</p>
+                <p>Pour agir l'association compte environ 600 adhérents, 6 groupes thématiques (ornithologie, géologie, botanique, mycologie, zero pesticide/eau/agriculture, reserve naturel) et une equipe pluridisciplinaire de permanents et de bénévoles actifs.</p>
+            </div>
+        </section>
+
+        <footer class="footer">
+            <div class="footer_haut">
+                <div class="column">
+                    <a href="">FAQ</a>
+                    <a href="">Mentions Légales</a>
+                    <a href="">Confidentialité</a>
+                </div>
+                <div class="footer_menu">
+                    <p><a href="homepage.html">ACCUEIL</a>   <span class="barre-sepa">|</span>   <a href="homepage.html#link_actions">ACTIONS</a>   <span class="barre-sepa">|</span>   <a href="homepage.html#link_associations">ASSOCIATIONS</a>   <span class="barre-sepa">|</span>   <a href="homepage.html#link_dechets">DECHETS</a></p>                </div>                    
+                <div class="column">
+                    <div class="reseaux">
+                        <a href="https://twitter.com" target="_blank" title="lien vers twitter"><img src="images/logo_twitter.png" alt="logo twitter"></a>
+                        <a href="https://www.facebook.com" target="_blank" title="lien vers facebook"><img src="images/logo_facebook.png" alt="logo facebook"></a>
+                        <a href="https://www.instagram.com" target="_blank" title="lien vers instagram"><img src="images/logo_instagram.png" alt="logo instagram"></a>
+                    </div>
+                    <p class="follow">#Suivez-Nous !</p>      
+                </div>
+            </div>
+            <p class="copyright">© 2019 | Sylvain, Quentin, Léo, Adrien et Dimitri</p>
+        </footer>
+    </body>
+</html>
